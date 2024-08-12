44e9d1a75c94787dfa6bbd0570e022ed50fa8c16
PopCatRock <popcatroc@gmail.com>
2024-08-12 01:17:54 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { ChainId } from '@popswap/sdk'
 
 import { injected, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x6789f48F8ED4506362592Ca58E9c160d624932B6'
+export const ROUTER_ADDRESS = '0x81a6A11Df566e300f2aDf7AE2b86Ac3213eF1FCd'
 
 // a list of tokens by chain
 type ChainTokenList = {
