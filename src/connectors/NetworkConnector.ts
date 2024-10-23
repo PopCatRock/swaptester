6868b6dff90e4ea0057659f15b13d9e911a754d6
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-10-23 20:26:38 +0100

    Update NetworkConnector.ts

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -36,7 +36,7 @@ class MiniRpcProvider implements AsyncSendable {
   public readonly path: string
   public readonly batchWaitTimeMs: number
 
-  private nextId = 82
+  private nextId = 1213
   private batchTimeoutId: ReturnType<typeof setTimeout> | null = null
   private batch: BatchItem[] = []
 
